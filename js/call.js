70a22cef89510baeab25f8cd079f9422196da0b1
offeyicial <oluwafeyisayofummi@gmail.com>
2023-06-25 23:18:31 +0100

    yes

--- js/call.js
+++ js/call.js
@@ -1,22 +1,42 @@
-var localStream;
-var remoteStream;
-let peerConnection
+let localStream;
+let remoteStream;
+let peerConnection;
 
 // my stream
 let init = async () => {
     localStream = await navigator.mediaDevices.getUserMedia({
         video: true,
         audio: true
-    })
-    document.getElementById(UserId).srcObject = localStream
+    });
+    document.getElementById(UserId).srcObject = localStream;
+    createOffer();
 }
 
-// other user's sream
+// other user's stream
 let createOffer = async () => {
     peerConnection = new RTCPeerConnection();
 
     remoteStream = new MediaStream();
-    document.getElementById('userB').srcObject = remoteStream;
+    document.getElementById(userB).srcObject = remoteStream;
+
+    // passing local stream
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    });
+
+    // when peer has its tracks
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track);
+        });
+    };
+
+    // generate ICE candidates
+    peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+            console.log('New ICE candidate:', event.candidate);
+        }
+    };
 
     // Creating offer
     let offer = await peerConnection.createOffer();
@@ -24,10 +44,17 @@ let createOffer = async () => {
 
     // Log the offer
     console.log('Offer:', offer);
+
+    // Listen for ICE candidates
+    peerConnection.onicecandidate = (event) => {
+        if (event.candidate) {
+            console.log('New ICE candidate:', event.candidate);
+        } else {
+            console.log('ICE candidate gathering complete.');
+        }
+    };
 }
 
-// Call the createOffer function to initiate the offer creation
-createOffer();
+// Call the init function to start the process
+init();
 
-
-init()
